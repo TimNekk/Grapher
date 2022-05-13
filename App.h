8354f34b4a27395fecd9318166d0f6a730cbe9b8
Tim <herew26@gmail.com>
2022-05-14 01:36:39 +0300

    Incidence matrix input and converting added

--- App.h
+++ App.h
@@ -62,9 +62,13 @@ public:
                 std::cout << "Representation as Adjacency matrix\n\n" << AdjacencyMatrix::FromGraph(graph);
                 break;
             }
+            case 4:
+            {
+                std::cout << "Representation as Incidence matrix\n\n" << IncidenceMatrix::FromGraph(graph);
+                break;
+            }
         }
     }
-
 
     static AdjacencyMatrix GetAdjacencyMatrix() {
         unsigned int vertex_count;
@@ -86,6 +90,60 @@ public:
         }
 
         AdjacencyMatrix graph {edges};
+        return graph;
+    }
+
+    static IncidenceMatrix GetIncidenceMatrix() {
+        unsigned int vertex_count, edges_count;
+        int exist;
+        std::set<Edge> edges;
+        std::vector<std::vector<int>> matrix;
+
+        std::cout << "Enter the number of vertices:\n";
+        std::cin >> vertex_count;
+        std::cout << "Enter the number of edges/arcs:\n";
+        std::cin >> edges_count;
+        std::cout << "Enter the incidence matrix of size (" << vertex_count << "x" << edges_count << "):\n";
+
+        for (unsigned int i = 0; i < vertex_count; ++i) {
+            std::vector<int> row;
+            row.reserve(edges_count);
+            for (int j = 0; j < edges_count; ++j) {
+                row.push_back(0);
+            }
+            matrix.push_back(row);
+        }
+
+        for (unsigned int i = 0; i < vertex_count; ++i) {
+            for (unsigned int j = 0; j < edges_count; ++j) {
+                std::cin >> exist;
+                matrix[i][j] = exist;
+            }
+        }
+
+        for (unsigned int j = 0; j < edges_count; ++j) {
+            unsigned int exist_start = 0, exist_start2 = 0, exist_end = 0;
+            for (unsigned int i = 0; i < vertex_count; ++i) {
+                if (matrix[i][j] == 1) {
+                    if (exist_start == 0) {
+                        exist_start = i + 1;
+                    } else {
+                        exist_start2 = i + 1;
+                    }
+                } else if (matrix[i][j] == -1) {
+                    exist_end = i + 1;
+                }
+            }
+
+            if (exist_end == 0) {
+                edges.insert({exist_start, exist_start2});
+                edges.insert({exist_start2, exist_start});
+            } else {
+                edges.insert({exist_start, exist_end});
+            }
+        }
+
+        IncidenceMatrix graph {edges};
         return graph;
     }
 };
