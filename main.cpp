2342edcaa363b57dd47d3f9e72dfd45ff32b0f3a
TimNekk <herew26@gmail.com>
2022-05-15 13:57:57 +0300

    app start added

--- main.cpp
+++ main.cpp
@@ -1,36 +1,6 @@
 #include "App.h"
 
 int main() {
-    App::ShowMenu();
-
-//    std::set<Edge> edges {
-//            {1, 2},
-//            {1, 4},
-//            {2, 3},
-//            {2, 4},
-//            {3, 2},
-//            {4, 1},
-//            {4, 3},
-//    };
-
-//    std::set<Edge> edges {
-//            {1, 2},
-//            {2, 1},
-//            {3, 4},
-//            {4, 3},
-//    };
-
-
-//    EdgesList graph {edges};
-
-//    std::cout << graph;
-
-//    AdjacencyMatrix graph = App::GetAdjacencyMatrix();
-//    App::ShowMenu(graph);
-//
-//    unsigned int action;
-//    std::cin >> action;
-//    App::DoAction(action, graph);
-
-
+    App app {"../input.txt", "../output.txt"};
+    app.Start();
 }
